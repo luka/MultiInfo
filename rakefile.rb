9205282dff36b97b4e71e32440f8e9212b7a482c
luka <luka@weblify.pl>
2009-07-28 11:04:09 +0200

    gemspec corrected

---
+++ rakefile.rb
@@ -0,0 +1,18 @@
+begin
+  require 'jeweler'
+  require File.join(File.dirname(__FILE__), *%w[lib/multiinfo/version])
+
+  Jeweler::Tasks.new do |gemspec|
+    gemspec.name = "multiinfo"
+    gemspec.version = MultiInfo::VERSION.to_s
+    gemspec.summary = "Ruby interface to the Polkomtel's MultiInfo SMS gateway service, based on Luke Redpath's clickatel library"
+    gemspec.description = "This gem is Ruby interface to the Polkomtel's MultiInfo SMS gateway service, based on Luke Redpath's clickatel library.
+    To use this gem, you will need an access to Polkomtel's MultiInfo service. You need user, password, sercvice_id and certificate for ssl connection.
+    Contact Polkomtel sales reps for info how to obtain acces to MultiInfo service."
+    gemspec.email = "luka@weblify.pl"
+    gemspec.homepage = "http://github.com/luka/multiinfo"
+    gemspec.authors = ["Josh Nichols"]    
+  end
+rescue LoadError
+  puts "Jeweler not available. Install it with: sudo gem install technicalpickles-jeweler -s http://gems.github.com"
+end
