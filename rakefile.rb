9db08a25cd60b8592c1af333195be42e4c0c78cd
luka <luka@weblify.pl>
2009-07-28 23:00:38 +0200

    minor changes to rdoc descriptions

--- rakefile.rb
+++ rakefile.rb
@@ -1,10 +1,8 @@
 begin
   require 'jeweler'
-  require File.join(File.dirname(__FILE__), *%w[lib/multiinfo/version])
 
   Jeweler::Tasks.new do |gemspec|
     gemspec.name = "multiinfo"
-    gemspec.version = MultiInfo::VERSION.to_s
     gemspec.summary = "Ruby interface to the Polkomtel's MultiInfo SMS gateway service, based on Luke Redpath's clickatel library"
     gemspec.description = "This gem is Ruby interface to the Polkomtel's MultiInfo SMS gateway service, based on Luke Redpath's clickatel library.
     To use this gem, you will need an access to Polkomtel's MultiInfo service. You need user, password, sercvice_id and certificate for ssl connection.
